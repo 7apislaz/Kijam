7293114b180a13b1db82c2b5df06f458afa0b0dc
7apislaz <belltech0210@gmail.com>
2023-01-30 18:51:00 +0900

    [UPDATE] 키잼 DB 플러터 임포트

--- lib/db/dbHelper.dart
+++ lib/db/dbHelper.dart
@@ -12,62 +12,104 @@ class DBHelper {
     return _db;
   }
 
-  static void _createDb(Database db) {
-    db.execute(
-      "CREATE TABLE Kid(key INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+  static void _createDb(Database db) async{
+    await db.execute(
+      "CREATE TABLE Kindergarten(key INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                "name TEXT, "
+                                "update_datetime NUMERIC);",
+    );
+    await db.execute(
+      "CREATE TABLE infant(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                          "inf_key TEXT, "
+                          "tag TEXT, "
+                          "name TEXT, "
+                          "gender TEXT , "
+                          "age INTEGER, "
+                          // "FOREIGN KEY(group) REFERENCES Kindergarten(key),"
+                          "picture TEXT,"
+                          "update_datetime NUMERIC, "
+                          "is_active TEXT CHECK(name IN('True', 'False')) );",
+    );
+    await db.execute(
+      "CREATE TABLE measured_data(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "height TEXT,"
+          "weight TEXT,"
+          "measured_datetime NUMERIC,"
+          "inf_key TEXT);",
+    );
+    await db.execute(
+      "CREATE TABLE emotion_data("
+          "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "selected_play TEXT, "
+          "emotion TEXT, "
+          "measured_datetime NUMERIC, "
+          "inf_key TEXT);",
+    );
+    await db.execute(
+      "CREATE TABLE attendance(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "attendance_type TEXT ,"
+          "emotion TEXT,"
+          "measured_datetime NUMERIC,"
+          "inf_key TEXT);",
     );
   }
-  Future<void> insertKid(Kid kid) async {
+  Future<void> insertInfant(Infant infant) async {
     final db = await database;
 
-    await db.insert("Kid", kid.toMap(),
+    await db.insert("Infant", infant.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
-  Future<List<Kid>> getAllKid() async {
+  Future<List<Infant>> getAllInfant() async {
     final db = await database;
 
-    final List<Map<String, dynamic>> maps = await db.query('Kid');
+    final List<Map<String, dynamic>> maps = await db.query('Infant');
 
     return List.generate(maps.length, (i) {
-      return Kid(
-        key: maps[i]['key'],
+      return Infant(
+        inf_key: maps[i]['inf_key'],
+        tag: maps[i]['tag'],
         name: maps[i]['name'],
+        gender: maps[i]['gender'],
         age: maps[i]['age'],
+        group: maps[i]['group'],
+        picture: maps[i]['picture'],
+        update_datetime: maps[i]['update_datetime'],
+        is_active: maps[i]['is_active'],
       );
     });
   }
 
-  Future<dynamic> getKid(int key) async {
+  Future<dynamic> getInfant(String inf_key) async {
     final db = await database;
 
     final List<Map<String, dynamic>> maps = (await db.query(
-      'Kid',
-      where: 'key = ?',
-      whereArgs: [key],
+      'Infant',
+      where: 'infant = ?',
+      whereArgs: [inf_key],
     ));
 
     return maps.isNotEmpty ? maps : null;
   }
 
-  Future<void> updateKid(Kid kid) async {
+  Future<void> updateInfant(Infant infant) async {
     final db = await database;
 
     await db.update(
-      "Kid",
-      kid.toMap(),
-      where: "key = ?",
-      whereArgs: [kid.key],
+      "Infant",
+      infant.toMap(),
+      where: "inf_key = ?",
+      whereArgs: [infant.inf_key],
     );
   }
 
-  Future<void> deleteKid(int key) async {
+  Future<void> deleteInfant(String inf_key) async {
     final db = await database;
 
     await db.delete(
-      "Kid",
-      where: "key = ?",
-      whereArgs: [key],
+      "Infant",
+      where: "inf_key = ?",
+      whereArgs: [inf_key],
     );
   }
 }
