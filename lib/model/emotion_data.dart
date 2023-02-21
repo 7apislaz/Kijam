c813f13fcccbcc8e79bf046dce5fda446669a8b1
7apislaz <belltech0210@gmail.com>
2023-02-21 18:04:10 +0900

    [UPDATE] id 추가

--- lib/model/emotion_data.dart
+++ lib/model/emotion_data.dart
@@ -1,12 +1,10 @@
 class EmotionData {
-  final String id;
   final String selected_play;
   final String measured_datetime;
   final String emotion;
   final String inf_key;
 
   EmotionData({
-    required this.id,
     required this.selected_play,
     required this.measured_datetime,
     required this.emotion,
@@ -15,7 +13,6 @@ class EmotionData {
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'selected_play': selected_play,
       'measured_datetime': measured_datetime,
       'emotion': emotion,
@@ -25,6 +22,6 @@ class EmotionData {
 
   @override
   String toString() {
-    return 'Emotion_data{id: $id, selected_play: $selected_play, measured_datetime: $measured_datetime, emotion: $emotion, inf_key: $inf_key}';
+    return 'Emotion_data{selected_play: $selected_play, measured_datetime: $measured_datetime, emotion: $emotion, inf_key: $inf_key}';
   }
 }
