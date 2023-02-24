d2443aa8d1560580805a0df4972a853cabe44ca8
7apislaz <belltech0210@gmail.com>
2023-02-24 18:07:26 +0900

    [UPDATE] 새 페이지 루트 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kijam_vision21tech/screens/kijam_setting_screen.dart';
+import 'package:kijam_vision21tech/screens/kindergartens_list_screen.dart';
 import 'package:kijam_vision21tech/screens/splash/splash_screen.dart';
 import 'package:kijam_vision21tech/screens/welcome_screen.dart';
 
@@ -7,4 +8,5 @@ final Map<String, WidgetBuilder> route = {
   WelcomeScreen.routeName: (context) => WelcomeScreen(),
   KijamSettingScreen.routeName: (context) => KijamSettingScreen(),
   SplashScreen.routeName: (context) => SplashScreen(),
+  KindergartensListScreen.routeName: (context) => KindergartensListScreen(),
 };
